af91f31799768844f06eee269d1a5ab6c65decf3
Alejandro Coronado <alejandro.coronado.n@gmail.com>
2023-01-12 15:32:58 -0600

    Original model

--- naivebayes.py
+++ naivebayes.py
@@ -1,27 +1,48 @@
-%matplotlib inline
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-import sklearn.datasets import fetch_20newgroups
+from  sklearn.datasets import fetch_20newsgroups
 
-from sklearn.feature_extraction.text import TfidVectorizer
-from sklearn.naivebayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
-
 from sklearn.metrics import confusion_matrix
 
- 
-mat = confusion_matrix(test.taget, labels)
-sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
-            xticklabels =train.target_names,
-            yticklabels = train.target_names)
-
-data = fetch_20newgroups()
+data = fetch_20newsgroups()
 data.target_names
 
+categories  = ['alt.atheism',
+ 'comp.graphics',
+ 'comp.os.ms-windows.misc',
+ 'comp.sys.ibm.pc.hardware',
+ 'comp.sys.mac.hardware',
+ 'comp.windows.x',
+ 'misc.forsale',
+ 'rec.autos',
+ 'rec.motorcycles',
+ 'rec.sport.baseball',
+ 'rec.sport.hockey',
+ 'sci.crypt',
+ 'sci.electronics',
+ 'sci.med',
+ 'sci.space',
+ 'soc.religion.christian',
+ 'talk.politics.guns',
+ 'talk.politics.mideast',
+ 'talk.politics.misc',
+ 'talk.religion.misc']
 
-model = make_pipeline(TfidVectorizer(), MultinomialNB())
+train  = fetch_20newsgroups(subset ="train", categories =categories)
+test = fetch_20newsgroups(subset = "test", categories=categories)
+
+#Convert column to list
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 model.fit(train.data, train.target)
 labels = model.predict(test.data)
 
 
+mat = confusion_matrix(test.taget, labels)
+
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels =train.target_names,
+            yticklabels = train.target_names)
